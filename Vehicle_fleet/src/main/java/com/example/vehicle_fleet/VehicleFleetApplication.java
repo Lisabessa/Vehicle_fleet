46332220fefba98abc42059c2b7e880d4a6cac63
Elizaveta Plavinskaya <lisabessa.neyz@gmail.com>
2025-06-03 17:13:12 +0700

    Project initialization

---
+++ Vehicle_fleet/src/main/java/com/example/vehicle_fleet/VehicleFleetApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleFleetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleFleetApplication.class, args);
+    }
+
+}
---
+++ Vehicle_fleet/src/test/java/com/example/vehicle_fleet/VehicleFleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_fleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleFleetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
