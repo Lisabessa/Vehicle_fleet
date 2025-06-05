d837b226ad4facabb84ea820a4cd476bb2b6db2c
Elizaveta Plavinskaya <lisabessa.neyz@gmail.com>
2025-06-05 17:24:34 +0700

    Show info from db

--- Vehicle_fleet/src/main/java/com/example/vehicle_fleet/AppController.java
+++ Vehicle_fleet/src/main/java/com/example/vehicle_fleet/AppController.java
@@ -1,14 +1,27 @@
 package com.example.vehicle_fleet;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class AppController {
 
+    @Autowired
+    private CarService carService;
+
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index(Model model, @Param("keyword") String keyword) {
+        ModelAndView mav = new ModelAndView("index");
+        List<Car> listCars = carService.listAll(keyword);
+        mav.addObject("listEntities", listCars);
+        mav.addObject("keyword", keyword);
+        return mav;
     }
 
 }
